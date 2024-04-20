213111a91bd456c21d719c052b4d9b77c219347d
wnwoals0127 <wnwoals0127@gmail.com>
2024-04-20 22:22:38 +0900

    Fix: span didn't initialized self->prev

--- span_init.c
+++ span_init.c
@@ -2,6 +2,7 @@
 #include "gpi_private.h"
 
 t_span	*span_init(t_span *self, t_trace *left, t_trace *right, i32 min_y, i32 max_y) {
+	self->prev = NULL;
 	self->next = NULL;
 	self->left = left;
 	self->right = right;
