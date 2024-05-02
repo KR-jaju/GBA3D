3ad98e099883ec3626fed5a20e1e061ccebfe88f
wnwoals0127 <wnwoals0127@gmail.com>
2024-05-02 15:05:16 +0900

    Feat: span

--- span_init.c
+++ span_init.c
@@ -2,66 +2,26 @@
 #include "gpi_private.h"
 
 t_span	*span_init(t_span *self, t_triangle *triangle) {
-	t_vertex*const	a = vertices[0];
-	t_vertex*const	b = vertices[1];
-	t_vertex*const	c = vertices[2];
-	i32 const		dx[] = {b->x - a->x, c->x - a->x, c->x - b->x};
-	i32 const		dy[] = {b->y - a->y, c->y - a->y, c->y - b->y};
+	i32 const	x[3] = {
+		triangle->vertices[0]->x,
+		triangle->vertices[1]->x,
+		triangle->vertices[2]->x
+	};
+	i32 const	y[3] = {
+		triangle->vertices[0]->y,
+		triangle->vertices[1]->y,
+		triangle->vertices[2]->y
+	};
+	i32 const	ac_orientation = (x[1] > x[2]) ? 1 : 0;
 
-
-	self->ref = triangle;
+	trace_init(&self->trace[!ac_orientation], x[0], y[0], x[1], y[1]);
+	trace_init(&self->trace[ac_orientation], x[0], y[0], x[2], y[2]);
 	self->prev = NULL;
 	self->next = NULL;
-	self->y_start = ((a->y + 3) & ~0b111) >> 3; //TODO: &연산이 필요하지 않을것으로 보임
-	self->y_center = ((b->y + 3) & ~0b111) >> 3;
-	self->y_end = ((c->y + 3) & ~0b111) >> 3;
-	self->ref = triangle;
-	// TODO: normal설정하기
-
-	self->ref = triangle;
-	self->prev = NULL;
-	self->next = NULL;
-	if (dx[2] < 0) {
-		self->dx[0][0] = dx[1];
-		self->dx[0][1] = dx[0];
-		self->dx[1][0] = dx[1];
-		self->dx[1][1] = dx[2];
-		self->dy[0][0] = dy[1];
-		self->dy[0][1] = dy[0];
-		self->dy[1][0] = dy[1];
-		self->dy[1][1] = dy[2];
-	} else {
-		self->dx[0][0] = dx[0];
-		self->dx[0][1] = dx[1];
-		self->dx[1][0] = dx[2];
-		self->dx[1][1] = dx[1];
-		self->dy[0][0] = dy[0];
-		self->dy[0][1] = dy[1];
-		self->dy[1][0] = dy[2];
-		self->dy[1][1] = dy[1];
-	}
-	i32 const	cy = ((a->y + 3) & ~0b111) + 4;
-	i32 const	cx = ((a->x + 3) & ~0b111) + 4;
-	i32 const	x_sign = (self->dx[0][0] < 0) ? -1 : 1;
-
-	self->error[0] += self->dx[0][0] * (cy - a->y) - self->dy[0][0] * (cx - a->x);
-	self->error[1] += self->dx[0][1] * (cy - a->y) - self->dy[0][1] * (cx - a->x);
-	while (self->error[0] < 0 || dy[0] * 8 <= self->error[0]) {
-		self->error[0] += dy[0] * x_sign * 8;
-		self->x[1] += x_sign;
-	}
-	while (self->error[1] < 0 || dy[1] * 8 <= self->error[1]) {
-		self->error[1] += dy[1] * x_sign * 8;
-		self->x[1] += x_sign;
-	}
-
-
-
-
-
-
-
-
-
+	self->triangle = triangle;
+	self->ac_orientation = ac_orientation;
+	self->entry = y[0];
+	self->breakpoint = y[1];
+	self->exit = y[2];
 	return (self);
 }
--- span_move.c
+++ span_move.c
@@ -1,86 +1,16 @@
 
 #include "gpi_private.h"
 
-/*
+void	span_move(t_span *self, i32 y) {
+	i32 const		ac_orientation = self->ac_orientation;
+	t_vertex*const	v1 = self->triangle->vertices[1];
+	t_vertex*const	v2 = self->triangle->vertices[2];
 
-Edge(AB, AP)
-AB.x * AP.y - AB.y * AP.x
-
-Edge(AB, AP) < 0 == 삼각형 내부
-
-
-w0 = Edge(AB, AP)
-nw0 = w0 / area
-
-cres = nw0 * c0 + nw1 * c1 + nw2 * c2
-or
-cres = nw0 / z0 * c0 + nw1 / z1 * c1 + nw2 / z2 * c2
-invz = nw0 / z0 + nw1 / z1 + nw2 / z2
-cres /= invz
-결론 : nwn / zn 을 구하면 된다
-
-nwn / zn = wn / zn / area
-dw/d(P.x) = -AB.y
-dw/d(P.y) = AB.x
-
-
-*/
-
-static
-i32	clamp(i32 a) {
-	if (a < 0)
-		return (0);
-	if (a >= 8)
-		return (8);
-	return (a);
-}
-
-void	span_move(t_span *self) {
-	// i32 const		is_upper = y < self->y[1];
-	i32 const		x_old = self->x[0];
-	i32*const		dwdx = self->ref->dwdx;
-	i32*const		dwdy = self->ref->dwdy;
-	i32 const		y = (*self->y << 3) + 4;
-	i32 const		step[2] = {
-		clamp(self->y_center - y),
-		clamp(y + 8 - self->y_center)
-	};
-	i32 const		up = step[0] < 4;
-	i32 const		x_sign = (self->dx[up][0] < 0) ? -1 : 1;
-	i32 const		*dy = self->dy[up];
-	i32				dx;
-
-	self->error[0] += step[0] * self->dx[0][0] + step[1] * self->dx[1][0];
-	self->error[1] += step[0] * self->dx[0][1] + step[1] * self->dx[1][1];
-	while (self->error[0] < 0 || dy[0] * 8 <= self->error[0]) {
-		self->error[0] += dy[0] * x_sign * 8;
-		self->x[1] += x_sign;
+	if (y != self->breakpoint) {
+		trace_move(&self->trace[0]);
+		trace_move(&self->trace[1]);
+		return ;
 	}
-	while (self->error[1] < 0 || dy[1] * 8 <= self->error[1]) {
-		self->error[1] += dy[1] * x_sign * 8;
-		self->x[1] += x_sign;
-	}
-
-
-
-	// i32 const		a_z = self->vertex[0]->z;
-	// i32 const		b_z = self->vertex[1]->z;
-	// i32 const		c_z = self->vertex[2]->z;
-	// i32 const		ab_x = self->vertex[1]->x - self->vertex[0]->x;
-	// i32 const		ab_y = self->vertex[1]->y - self->vertex[0]->y;
-	// i32 const		bc_x = self->vertex[2]->x - self->vertex[1]->x;
-	// i32 const		bc_y = self->vertex[2]->y - self->vertex[1]->y;
-	// i32 const		ca_x = self->vertex[0]->x - self->vertex[2]->x;
-	// i32 const		ca_y = self->vertex[0]->y - self->vertex[2]->y;
-	// TODO: precalculate, 8 precision vs 1precision
-
-	// trace_move(left);
-	// trace_move(right);
-	// self->weight[0] += (bc_x - bc_y * (left->x - x_old)) * a_z;
-	// self->weight[1] += (ca_x - ca_y * (left->x - x_old)) * b_z;
-	// self->weight[2] += (ab_x - ab_y * (left->x - x_old)) * c_z;
-	dx = self->x[0] - x_old;
-	self->w[0] += dwdx[0] * dx + dwdy[0];
-	self->w[0] += dwdx[1] * dx + dwdy[1];
-	self->w[0] += dwdx[2] * dx + dwdy[2];
+	trace_init(&self->trace[!ac_orientation], v1->x, v1->y, v2->x, v2->y);
+	trace_move(&self->trace[ac_orientation]);
 }
