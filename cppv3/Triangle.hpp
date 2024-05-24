2cf3b4c076537db86ed3a1079913f4f7e4b8c430
KR-jaju <wnwoals0127@gmail.com>
2024-05-24 19:31:07 +0900

    Feat: Triangle::sort

--- cppv3/Triangle.hpp
+++ cppv3/Triangle.hpp
@@ -3,10 +3,23 @@
 # define TRIANGLE_HPP
 
 # include "Vertex.hpp"
+# include <algorithm>
 
 struct Triangle {
 	Vertex	vertex[3];
 	// 정렬기준: y 작을수록 앞, y가 같으면 x가 작은게 앞
+	void	sort() {
+		Vertex	&v0 = this->vertex[0];
+		Vertex	&v1 = this->vertex[1];
+		Vertex	&v2 = this->vertex[2];
+
+		if (v0.y > v1.y || (v0.y == v1.y && v0.x > v1.x))
+			std::swap(v0, v1);
+		if (v1.y > v2.y || (v1.y == v2.y && v1.x > v2.x))
+			std::swap(v1, v2);
+		if (v0.y > v1.y || (v0.y == v1.y && v0.x > v1.x))
+			std::swap(v0, v1);
+	}
 };
 
 #endif
