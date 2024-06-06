c3d130b9a06fab4b523ec73f84ef0eca84fd69ac
wnwoals0127 <wnwoals0127@gmail.com>
2024-06-06 13:11:04 +0900

    Refactor: simple comment removal

--- source/main.cpp
+++ source/main.cpp
@@ -9,13 +9,6 @@ COLOR *vid_page= vid_mem_back;
 int	main() {
 	// *(u16 *)(0x04000000) = 0b0000010000000011;
 	REG_DISPCNT = DCNT_MODE4 | DCNT_BG2;
-	// Mesh<4, 2>		mesh = {
-	// 	{{0, 100, 0, 0, 0},
-	// 	{234, 145, 0, 0xFFFF, 0},
-	// 	{560, 634, 0, 0, 0xFFFF},
-	// 	{650, 400, 0, 0xFFFF, 0xFFFF}},
-	// 	{1, 2, 3, 0, 1, 2}
-	// };
 	Mesh<4, 2>		mesh = {
 		{{0.0f, 0.8f, 6.0f, 0, 0},
 		{-0.8f, 0.0f, 6.0f, 0xFFFF, 0},
@@ -35,27 +28,3 @@ int	main() {
 		vid_flip();	
 	}
 }
-// #include <stdio.h>
-// #include "debug.hpp"
-// #include "clock.hpp"
-// int	main(void) {
-// 	clock_init();
-// 	debug_init();
-// 	Mesh<4, 2>		mesh = {
-// 		{{0, 204, 1000, 0, 0},
-// 		{-204, 0, 1000, 0xFFFF, 0},
-// 		{0, -204, 1000, 0, 0xFFFF},
-// 		{204, 0, 1000, 0xFFFF, 0xFFFF}},
-// 		{0, 1, 2, 0, 2, 3}
-// 	};
-// 	Camera	camera;
-
-// 	camera.update();
-// 	camera.push(mesh);
-// 	debug_view();
-// 	while(true) {
-// 		// camera.render((u8 *)0x06000F00);
-// 		debug_offset = 0;
-// 		// debug_view();
-// 	}
-// }
