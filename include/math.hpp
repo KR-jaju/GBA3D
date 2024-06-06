13106d6a0060ccee4c97a7b68a62b36550f9bd2e
wnwoals0127 <wnwoals0127@gmail.com>
2024-06-06 15:47:23 +0900

    Refactor: math.hpp revision 2

--- include/math.hpp
+++ include/math.hpp
@@ -3,7 +3,6 @@
 # define MATH_HPP
 
 # include <cmath>
-
 # include "fixed.hpp"
 # include "util.hpp"
 
@@ -13,10 +12,44 @@
 # define ONE_DEGREE  0xB6
 # define FIXED_12_SCALE (1 << 12)
 
-fixed	sqrt(fixed f);
+static INLINE fixed	sqrt(fixed f) {
+	u64 const	a = u64(f.num) << FIX_SHIFT;
+	u64		l = 0;
+	u64		r = u64(f.num) + (1 << FIX_SHIFT);
+	u64   	m;
 
-fixed	abs(fixed f);
+	while(l != r - 1) {
+		m = (l + r) >> 1;
+		if (m * m <= a)
+			l = m;
+		else
+			r = m;
+	}
+	return (fixed::from(l));
+}
 
-void	sincos(int angle, fixed &sine, fixed &cosine);
+static INLINE fixed	abs(fixed f) {
+	if (f > 0)
+		return (f);
+	return (-f);
+}
+
+static INLINE void	sincos(int angle, fixed &sine, fixed &cosine) {
+	angle = angle << 16 >> 16;
+	int shift = (angle ^ (angle << 1)) & 0xC000;
+	int x = (angle + shift) << 17 >> 17;
+	fixed c = fixed::from((1 << FIX_SHIFT) - x * x / 3448); // TODO: div
+	
+	fixed s = sqrt(fixed(1) - c * c);
+
+	if (shift & 0x4000)
+		swap(c, s);
+	if (angle < 0)
+		s = -s;
+	if (shift & 0x8000)
+		c = -c;
+	cosine = c;
+	sine = s;
+}
 
 #endif
