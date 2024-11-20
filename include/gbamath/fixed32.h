4c1f1f16ae38b3ec2b21ae1d2e2aa51779bc52e1
KR-jaju <wnwoals0127@gmail.com>
2024-11-21 02:00:51 +0900

    Typo: f32->fixed32

--- include/gbamath/fixed32.h
+++ include/gbamath/fixed32.h
@@ -1,16 +1,16 @@
 #pragma once
 #include "common.h"
 
-typedef struct f32 f32;
+typedef struct fixed32 f32;
 
-struct f32
+struct fixed32
 {
 	constexpr static i32 FIX_SHIFT = 16;
 	i32 num;
 	
-	f32() = default;
-	constexpr	f32(i32 d): num(d << FIX_SHIFT) {}
-	constexpr	f32(float f): num(f * (1 << FIX_SHIFT)) {}
+	fixed32() = default;
+	constexpr	fixed32(i32 d): num(d << FIX_SHIFT) {}
+	constexpr	fixed32(float f): num(f * (1 << FIX_SHIFT)) {}
 
 	constexpr explicit operator	float() const {
 		return float(num) / (1 << FIX_SHIFT);
@@ -19,24 +19,24 @@ struct f32
 		return (num >> FIX_SHIFT);
 	}
 	
-	constexpr f32	operator-() const {
-		return f32::from(-this->num);
+	constexpr fixed32	operator-() const {
+		return fixed32::from(-this->num);
 	}
 
-	constexpr f32	operator+(f32 other) const {
-		return f32::from(num + other.num);
+	constexpr fixed32	operator+(fixed32 other) const {
+		return fixed32::from(num + other.num);
 	}
-	constexpr f32	operator-(f32 other) const {
-		return f32::from(num - other.num);
+	constexpr fixed32	operator-(fixed32 other) const {
+		return fixed32::from(num - other.num);
 	}
-	constexpr f32   operator*(f32 other) const {
-		return f32::from(i32((i64(num) * i64(other.num)) >> FIX_SHIFT));
+	constexpr fixed32   operator*(fixed32 other) const {
+		return fixed32::from(i32((i64(num) * i64(other.num)) >> FIX_SHIFT));
 	}
-	constexpr f32	operator>>(i32 shift) const {
-		return f32::from(this->num >> shift);
+	constexpr fixed32	operator>>(i32 shift) const {
+		return fixed32::from(this->num >> shift);
 	}
-	constexpr f32	operator<<(i32 shift) const {
-		return f32::from(this->num << shift);
+	constexpr fixed32	operator<<(i32 shift) const {
+		return fixed32::from(this->num << shift);
 	}
 	template <typename T>
 	bool    operator<(T other) const {
@@ -63,48 +63,48 @@ struct f32
 		return (this->num != (i32(other) << FIX_SHIFT));
 	}
 
-	constexpr bool	operator<(f32 other) const {
+	constexpr bool	operator<(fixed32 other) const {
 		return (this->num < other.num);
 	}
-	constexpr bool	operator>(f32 other) const {
+	constexpr bool	operator>(fixed32 other) const {
 		return (this->num > other.num);
 	}
-	constexpr bool	operator<=(f32 other) const {
+	constexpr bool	operator<=(fixed32 other) const {
 		return (this->num <= other.num);
 	}
-	constexpr bool	operator>=(f32 other) const {
+	constexpr bool	operator>=(fixed32 other) const {
 		return (this->num >= other.num);
 	}
-	constexpr bool	operator==(f32 other) const {
+	constexpr bool	operator==(fixed32 other) const {
 		return (this->num == other.num);
 	}
-	constexpr bool	operator!=(f32 other) const {
+	constexpr bool	operator!=(fixed32 other) const {
 		return (this->num != other.num);
 	}
 
-	constexpr f32	&operator+=(f32 other) {
+	constexpr fixed32	&operator+=(fixed32 other) {
 		*this = *this + other;
 		return (*this);
 	}
-	constexpr f32	&operator-=(f32 other) {
+	constexpr fixed32	&operator-=(fixed32 other) {
 		*this = *this - other;
 		return (*this);
 	}
-	constexpr f32	&operator*=(f32 other) {
+	constexpr fixed32	&operator*=(fixed32 other) {
 		*this = *this * other;
 		return (*this);
 	}
-	constexpr f32	&operator>>=(i32 shift) {
+	constexpr fixed32	&operator>>=(i32 shift) {
 		*this = *this >> shift;
 		return (*this);
 	}
-	constexpr f32	&operator<<=(i32 shift) {
+	constexpr fixed32	&operator<<=(i32 shift) {
 		*this = *this << shift;
 		return (*this);
 	}
 
-	static constexpr f32	from(i32 other) {
-		f32   ret;
+	static constexpr fixed32	from(i32 other) {
+		fixed32   ret;
 
 		ret.num = other;
 		return (ret);
