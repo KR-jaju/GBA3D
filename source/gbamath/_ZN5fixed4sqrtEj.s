f161338fffb3fc7f7d406a9ae9694a44e0c9993d
KR-jaju <wnwoals0127@gmail.com>
2025-03-30 15:42:42 +0900

    Fix: fixed::sqrt bug fixed

--- source/gbamath/_ZN5fixed4sqrtEj.s
+++ source/gbamath/_ZN5fixed4sqrtEj.s
@@ -20,11 +20,11 @@ _ZN5fixed4sqrtEj:
 	cmp		r14, r0, LSR #16 @ 높은 자리 비교
 	cmpeq	r12, r0, LSL #16 @ 낮은 자리 비교
 	@ 높은 자리 비교에서 우위가 정해지지 않을 경우, 낮은 자리까지 비교하기
-	movle	r1, r3 @ l = m
-	movgt	r2, r3 @ r = m
+	movls	r1, r3 @ l = m
+	movhi	r2, r3 @ r = m
 	add		r3, r1, r2
 	mov		r3, r3, LSR #1 @ r3 = (l + r) / 2
-	cmp		r1, r3
+	cmp		r1, r3 @ l != m
 	bne		.L0
 .L1:
 	mov		r0, r3
